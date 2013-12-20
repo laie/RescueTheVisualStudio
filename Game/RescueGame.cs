46baf36bd3afc3583b0a68b42833a4159619940f
Laie(Jee-heon Oh) <jpjps@naver.com>
2013-12-20 22:02:56 +0900

    first -- Farseer/MonoGame setting

---
+++ Game/RescueGame.cs
@@ -0,0 +1,115 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+using System.IO;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.DebugView;
+using FarseerPhysics.Factories;
+#endregion
+
+namespace TestMonoGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class RescueGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        World world;
+        DebugViewXNA debugWorldView;
+
+        Texture2D texture;
+
+        Camera2D camera;
+
+        Body coll;
+
+        public RescueGame()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // TODO: use this.Content to load your game content here
+            texture = Texture2D.FromStream(GraphicsDevice, new FileStream(@"C:\Users\Seiris\Desktop\painted.png", FileMode.Open));
+            world = new World(new Vector2(0, 1));
+            debugWorldView = new DebugViewXNA(world);
+            debugWorldView.LoadContent(GraphicsDevice, Content);
+            camera = new Camera2D(GraphicsDevice);
+            coll = BodyFactory.CreateRectangle(world, 1, 1, 1);
+            coll.BodyType = BodyType.Dynamic;
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            world.Step(Math.Min((float)gameTime.ElapsedGameTime.TotalSeconds, (1f / 30f)));
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, new Vector2(10, 10), Color.White);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+            debugWorldView.RenderDebugData(ref camera.SimProjection, ref camera.SimView);
+
+            base.Draw(gameTime);
+        }
+    }
+}
